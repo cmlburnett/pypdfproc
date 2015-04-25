7328800467be146a3d2cd1e337259298b29e47eb
Colin ML Burnett <cmlburnett@gmail.com>
2015-04-24 22:11:39 -0500

    Fix space vs. tab

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 """Installs pypdfproc using distutils
 
 Run:
-    python setup.py install
+	python setup.py install
 
 to install this package.
 """
 
 try:
-    from setuptools import setup
+	from setuptools import setup
 except ImportError:
-    from distutils.core import setup
+	from distutils.core import setup
 
 from distutils.command.install import INSTALL_SCHEMES
 from distutils.command.build_py import build_py
@@ -24,20 +24,23 @@ version = "1.0.0"
 desc = "PDF processor"
 long_desc = "Processes and updates PDF files specifically for journal articles and references"
 classifiers = [
-    "Intended Audience :: Developers",
-    "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.3",
+	"Intended Audience :: Developers",
+	"Programming Language :: Python :: 3",
+	"Programming Language :: Python :: 3.3",
 ]
 author = "Colin M Burnett"
 author_email = "cmlburnett@gmail.com"
 url = "http://www.candysporks.org"
 cp_license = "BSD"
 packages = [
-    "pypdfproc",
+	"pypdfproc",
 ]
 data_files = [
-    ('src', ['/foo',
-                  ]),
+	('src', [,
+	]),
+	('src/parser', ['src/parser/pdf.py',
+					'src/parser/text.py'
+	]),
 ]
 scripts = []
 
@@ -49,28 +52,29 @@ if sys.version_info >= (3, 0):
 ###############################################################################
 
 setup_params = dict(
-    name=name,
-    version=version,
-    description=desc,
-    long_description=long_desc,
-    classifiers=classifiers,
-    author=author,
-    author_email=author_email,
-    url=url,
-    license=cp_license,
-    packages=packages,
-    data_files=data_files,
-    scripts=scripts,
+	name=name,
+	version=version,
+	description=desc,
+	long_description=long_desc,
+	classifiers=classifiers,
+	author=author,
+	author_email=author_email,
+	url=url,
+	license=cp_license,
+	packages=packages,
+	data_files=data_files,
+	scripts=scripts,
 )
 
 def main():
-    if sys.version < required_python_version:
-        s = "I'm sorry, but %s %s requires Python %s or later."
-        print(s % (name, version, required_python_version))
-        sys.exit(1)
+	if sys.version < required_python_version:
+		s = "I'm sorry, but %s %s requires Python %s or later."
+		print(s % (name, version, required_python_version))
+		sys.exit(1)
 
-    setup(**setup_params)
+	setup(**setup_params)
 
 
 if __name__ == "__main__":
     main()
+
