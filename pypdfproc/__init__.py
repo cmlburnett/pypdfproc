86593232a718eb1bae051ff36dd611d22d4c4241
Colin ML Burnett <cmlburnett@gmail.com>
2015-04-26 12:00:19 -0500

    Clear yet another "pdf" class but this one intended to be the primary entrant point to parsing PDF files

--- pypdfproc/__init__.py
+++ pypdfproc/__init__.py
@@ -4,8 +4,59 @@ PDF Processor
 
 __version__ = "1.0.0"
 
-
 __all__ = ['parser']
 
+# System libs
+import mmap
+
 from . import parser
+from . import pdf as _pdf
+
+class PDF:
+	# File name, file object, and mmap object
+	fname = None
+	f = None
+	m = None
+
+	# PDF parser
+	p = None
+
+
+	def __init__(self, fname):
+		self.fname = fname
+
+		self.f = open(fname, 'rb')
+		self.m = mmap.mmap(self.f.fileno(), 0, prot=mmap.PROT_READ)
+
+		self.p = parser.PDFTokenizer(self.m)
+		self.p.Initialize()
+
+	def Close(self):
+		self.m.close()
+		self.f.close()
+
+	def GetRootObject(self):
+		return self.p.GetRootObject()
+
+	def GetFullText(self):
+		root = self.GetRootObject()
+
+		contents = []
+
+		pages = root.Pages.DFSPages()
+		for page in pages:
+			cts = page.Contents
+
+			pgct = None
+			if type(cts) == list:
+				contents += [ct.Stream for ct in cts]
+			else:
+				contents.append(cts.Stream)
+
+		content = "\n".join(contents)
+
+		tt = parser.TextTokenizer(self.f, self.p)
+
+		toks = tt.TokenizeString(content)
+		return toks
 
